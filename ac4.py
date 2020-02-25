873fbb9b79bda646ecc2b2ec8bca76254b39b7a6
MuriloCarlos1567 <efgs96@gmail.com>
2020-02-25 15:48:03 -0300

    added ac4

---
+++ ac4.py
@@ -0,0 +1,11 @@
+import math
+
+L = float(input())
+
+A = ((L*L)*(3**(1/2)))/4
+
+print("Lado do hexagono:" ,L ,"metros,")
+
+print("Area:" ,(A*6) ,"metros quadrados,")
+
+print("Perimetro:" ,(L*6) ,"metros.")
